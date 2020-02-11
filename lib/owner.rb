7c501ec83c23983dea99b6c37187e550f79fef6e
Adam G Horn <hornadam8@gmail.com>
2020-02-11 13:20:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,28 @@
 class Owner
+  
+  @@all = []
+  
   attr_reader :name, :species
   
   def initialize(name)
     @species = "human"
     @name = name
+    @@all << self
   end
   
   def say_species
     "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
   end
 end
